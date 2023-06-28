13fc8b0561a4d32f2e5134a824c68b3e6c9c57dd
Daniel Mendoza <daniel.mendozateixeira@gmail.com>
2023-06-28 09:47:35 -0300

    efeito de loading botao entrega

--- telas/Principal.tsx
+++ telas/Principal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, PermissionsAndroid, Image } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import { Linking } from 'react-native';
@@ -6,45 +6,49 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {salvarEntrega} from '../classes/Entrega';
 import { Button } from '@rneui/themed';
 
-async function requestLocationPermission(){
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Permissão de Localização',
-        message: 'Este aplicativo precisa de permissão para acessar sua localização.',
-        buttonNeutral: 'Perguntar depois',
-        buttonNegative: 'Cancelar',
-        buttonPositive: 'OK',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      // Permissão de localização concedida
-      getLocation();
-    } else {
-      // Permissão de localização negada
-      console.log('Permissão de localização negada');
-    }
-  } catch (error) {
-    console.warn(error);
-  }
-};
-
-async function getLocation() {
-  Geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-
-      salvarEntrega(latitude, longitude);
-    },
-    (error) => {
-      console.warn(error.code, error.message);
-    },
-    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
-  );
-};
-
 function Principal(){
+
+  const [btnEntregaLoading, setbtnEntregaLoading] = useState(false);
+
+  async function requestLocationPermission(){
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permissão de Localização',
+          message: 'Este aplicativo precisa de permissão para acessar sua localização.',
+          buttonNeutral: 'Perguntar depois',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // Permissão de localização concedida
+        getLocation();
+      } else {
+        // Permissão de localização negada
+        console.log('Permissão de localização negada');
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+  
+  async function getLocation() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setbtnEntregaLoading(true);
+        
+        salvarEntrega(latitude, longitude).then((r) => {setbtnEntregaLoading(false)});
+      },
+      (error) => {
+        console.warn(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+  };  
+
   return (
     <View
       style={{
@@ -59,7 +63,8 @@ function Principal(){
         }}
         source={require('../entregafeitafd2.png')}
       />
-      <Button
+      { !btnEntregaLoading ? 
+        <Button
         title="Entrega Feita"
         buttonStyle={{
           backgroundColor: '#1994FF',
@@ -74,6 +79,18 @@ function Principal(){
         }}
         titleStyle={{ fontWeight: 'bold', fontSize: 30 }}
         onPress={() => requestLocationPermission()}/>
+        :
+        <Button
+        loading
+        disabled
+        containerStyle={{
+          width: 300,
+          marginHorizontal: 50,
+          marginVertical: 100,
+        }}/>
+      
+      }
+      
 
         <Text style={{
         color: 'black',
